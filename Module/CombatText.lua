1dcf0d1dbb7de32e648e402c7e8c202ee99be821
cswxero <cswxero@naver.com>
2014-08-23 11:30:16 +0900

    최초 업로드

---
+++ Module/CombatText.lua
@@ -0,0 +1,172 @@
+﻿local _G = _G
+local unpack = unpack
+
+if (not ImprovedBlizzFrame.config.combattext) then
+    return
+end
+
+------- Combat Text
+local ASCT_X_POSITION = 0
+local ASCT_Y_POSITION = -200
+
+local size = 18
+
+
+-- true/false 로 켜고 끕니다.
+local CombatTextHighlightDB = {
+	-- 공통 버프
+	["<고대의 격분> 사라짐"] = true, -- 야수특성 사냥꾼 소환수
+	["<고대의 격분>"] = true,
+	["<고통 억제> 사라짐"] = true, -- 수양특성 사제
+	["<고통 억제>"] = true,
+	["<마력 주입> 사라짐"] = true, -- 수양특성 사제
+	["<마력 주입>"] = true,
+	["<속임수 거래> 사라짐"] = true, -- 도적
+	["<속임수 거래>"] = true,
+	["<수호 영혼> 사라짐"] = true, -- 신성특성 사제
+	["<수호 영혼>"] = true,
+	["<시간 왜곡> 사라짐"] = true, -- 마법사
+	["<시간 왜곡>"] = true,
+	["<신의 권능: 방벽> 사라짐"] = true, -- 수양특성 사제
+	["<신의 권능: 방벽>"] = true,
+	["<영웅심> 사라짐"] = true, -- 얼라이언스 주술사
+	["<영웅심>"] = true,
+	["<오라 숙련> 사라짐"] = true, -- 신성특성 성기사
+	["<오라 숙련>"] = true,
+	["<천상의 수호자> 사라짐"] = true, -- 보호특성 성기사
+	["<천상의 수호자>"] = true,
+	["<피의 욕망> 사라짐"] = true, -- 호드 주술사
+	["<피의 욕망>"] = true,
+	["<희생의 손길> 사라짐"] = true, -- 성기사
+	["<희생의 손길>"] = true,
+
+	-- 일반 이벤트
+	[ENTERING_COMBAT] = true,
+	[LEAVING_COMBAT] = true,
+	[HEALTH_LOW] = true,
+	[MANA_LOW] = true,
+};
+
+
+local CombatTextIgnoreDB = {
+	-- 무시할 이벤트의 이름 CombatTextHighlightDB 보다 우선적으로 적용하여 무시리스트에 등록된건 CombatTextHighlightDB에 등록되어있더라도 안나온다
+	["<test> 사라짐"] = true,
+};
+
+
+local CombatText_AddMessage_Org;
+
+local function combatText_AddMessage(message, scrollFunction, r, g, b, displayType, isStaggered)
+	if CombatTextHighlightDB[message] then
+		if not CombatTextIgnoreDB[message] then
+			CombatText_AddMessage_Org(message, scrollFunction, r, g, b, "crit", isStaggered);
+		end
+	else
+		if not CombatTextIgnoreDB[message] then
+			CombatText_AddMessage_Org(message, scrollFunction, r, g, b, displayType, isStaggered);
+		end
+	end
+end
+
+function onCombatTextMOD_Event(self, event, arg1)
+
+	local bShowHeal = nil
+	
+	if event == "PLAYER_ENTERING_WORLD" then--or event == "ACTIVE_TALENT_GROUP_CHANGED"then
+		if not CombatText_AddMessage_Org then 
+			--print(event.." - Not CombatText_AddMessage_Org ")
+			CombatText_AddMessage_Org = CombatText_AddMessage;
+			CombatText_AddMessage = combatText_AddMessage;
+			--hooksecurefunc("CombatText_AddMessage", combatText_AddMessage);
+		end	
+		
+		if CombatText_UpdateDisplayedMessages then
+			CombatTextFont:SetFont("Fonts\\2002.ttf",size,"OUTLINE")
+			--CombatTextFont:SetFont("Fonts\\K_Damage.TTF",size,"OUTLINE")
+			CombatTextFont:SetShadowOffset(1.25, -1.25)
+			CombatTextFont:SetShadowColor(0, 0, 0)
+			--CombatTextFont:SetShadowOffset(2, -2);
+			--CombatTextFont:SetShadowColor(0, 0, 0);
+			COMBAT_TEXT_HEIGHT = size;
+			COMBAT_TEXT_CRIT_MAXHEIGHT = floor(size * 3.6); -- 2.4
+			COMBAT_TEXT_CRIT_MINHEIGHT = floor(size * 2); -- 1.2
+			
+			--local masteryIndex = GetPrimaryTalentTree();
+
+			--if (masteryIndex == nil) then
+			--	bShowHeal = 1
+
+			--else
+			--	local id, name = GetTalentTabInfo(masteryIndex);
+
+			--	if (name == "1" or name == "2" or name == "3" or name == "4") then
+			--		ChatFrame1:AddMessage(name.."HIDE COMBAT TEXT.")
+			--		bShowHeal = nil
+			--	else
+			--		bShowHeal = 1
+			--	end
+			--end
+
+			--COMBAT_TEXT_TYPE_INFO["HEAL_CRIT"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			--COMBAT_TEXT_TYPE_INFO["HEAL"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			--COMBAT_TEXT_TYPE_INFO["PERIODIC_HEAL_ABSORB"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			--COMBAT_TEXT_TYPE_INFO["HEAL_CRIT_ABSORB"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			--COMBAT_TEXT_TYPE_INFO["HEAL_ABSORB"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			--COMBAT_TEXT_TYPE_INFO["PERIODIC_HEAL"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			
+			--도트힐 찍지않음
+			COMBAT_TEXT_TYPE_INFO["PERIODIC_HEAL"].show= ImprovedBlizzFrame.combattext.SHOW_PERIODIC_HEAL;
+			--COMBAT_TEXT_TYPE_INFO["PERIODIC_HEAL_ABSORB"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			--COMBAT_TEXT_TYPE_INFO["PERIODIC_HEAL"] = {r = 0.1, g = 1, b = 0.1, show = bShowHeal};
+			
+			hooksecurefunc("CombatText_UpdateDisplayedMessages", ASCT_UpdateDisplayedMessages);
+			
+			ASCT_UpdateDisplayedMessages()
+			
+		end
+		return;
+	end
+end 
+
+function ASCT_UpdateDisplayedMessages()
+
+  if ( COMBAT_TEXT_FLOAT_MODE == "1" ) then
+    COMBAT_TEXT_SCROLL_FUNCTION = CombatText_StandardScroll;
+    COMBAT_TEXT_LOCATIONS = {
+      startX = ASCT_X_POSITION,
+      startY = 460 * COMBAT_TEXT_Y_SCALE,
+	  --startY = 384 * COMBAT_TEXT_Y_SCALE,
+      endX = ASCT_X_POSITION,
+      endY = 650 * COMBAT_TEXT_Y_SCALE
+	  --endY = 609 * COMBAT_TEXT_Y_SCALE
+    };
+
+  elseif ( COMBAT_TEXT_FLOAT_MODE == "2" ) then
+    COMBAT_TEXT_SCROLL_FUNCTION = CombatText_StandardScroll;
+    COMBAT_TEXT_LOCATIONS = {
+      startX = ASCT_X_POSITION,
+      startY = 340 * COMBAT_TEXT_Y_SCALE,
+	  --startY = 384 * COMBAT_TEXT_Y_SCALE,
+      endX = ASCT_X_POSITION,
+      endY =  140 * COMBAT_TEXT_Y_SCALE
+	  --endY =  159 * COMBAT_TEXT_Y_SCALE
+    };
+  else
+    COMBAT_TEXT_SCROLL_FUNCTION = CombatText_FountainScroll;
+    COMBAT_TEXT_LOCATIONS = {
+      startX = ASCT_X_POSITION,
+      startY = 460 * COMBAT_TEXT_Y_SCALE,
+	  --startY = 384 * COMBAT_TEXT_Y_SCALE,
+      endX = ASCT_X_POSITION,
+      endY = 650 * COMBAT_TEXT_Y_SCALE
+	  --endY = 609 * COMBAT_TEXT_Y_SCALE
+    };
+  end
+  CombatText_ClearAnimationList();
+end
+
+CombatTextFrame = CreateFrame("Frame", nil, UIParent)
+CombatTextFrame :RegisterEvent("PLAYER_ENTERING_WORLD")
+--CombatTextFrame :RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+CombatTextFrame :RegisterEvent("PLAYER_LOGIN")
+CombatTextFrame :SetScript("OnEvent", onCombatTextMOD_Event)
---
+++ Module/xArena_PVPTrinkets.lua
@@ -0,0 +1,165 @@
+﻿if (ImprovedBlizzFrame.config.xArena_PVPTrinkets) then	
+	print("xArena_PVPTrinkets Module");
+	-- Chek Cooldown in Battleground
+	COOLDOWN_MAP={
+	--공통
+		[59752] = 120, --"인간의 자생력" (인간 종족 특성)
+		[42292] = 120, --"계급장"
+		[7744] = 120, --"포세이큰의 의지",
+	}
+	
+	cooldown={};	
+
+	function isReset(s,n)
+		if(RESET_MAP[s]) then 
+			for _,id in pairs(RESET_MAP[s]) do 
+				if(_G[id..n]) then 
+					rfc(_G[id..n].c,GetTime(),0,1)
+				end;
+			end;
+			
+			return 1;
+		end;
+	end;
+
+	function Cp(n,s)
+		f.t=f:CreateTexture(n,"BORDER")
+		
+		if(s==42292) then 
+			f.t:SetTexture(GetItemIcon(70395))
+		else 
+			f.t:SetTexture(GetSpellTexture(s))
+		end;
+		
+		f.t:SetSize(ps,ps)
+		f.t:SetPoint("Top",0,100)
+		f.t.c=cf("Cooldown",n.."C")
+		f.t.c:SetAllPoints(f.t)
+		
+		return f.t;
+	end
+
+	function isHarm(fl)
+		return bit.band(fl,COMBATLOG_OBJECT_REACTION_HOSTILE)
+	end;
+	
+	function isSpell(e)
+		return e=="SPELL_CAST_SUCCESS" or e=="SPELL_AURA_APPLIED" or e=="SPELL_MISSED" or e=="SPELL_SUMMON"
+	end;
+
+	function SC(n,s)
+		if not cooldown[n] then 
+			cooldown[n]={}
+		end;
+		
+		if not cooldown[n][s] then 
+			cooldown[n][s]=Cp(s..n,s)
+		end;
+		
+		rfc(_G[s..n].c,GetTime(),COOLDOWN_MAP[s],1)
+	end;
+	
+	function isTarget(n)
+		local tn=UnitName("target")
+		return(tn and n==strmatch(tn,"[%P]+"))
+	end
+
+	function Fp(n)
+		for i=1,WorldFrame:GetNumChildren()do 
+			frame=select(i,WorldFrame:GetChildren())
+			if (frame) then
+				if( frame:GetName() )then
+					
+					if(gsub(frame:GetName(), '%s%(%*%)',''):find("NamePlate")) then
+						if( n==strmatch(((select(2, frame:GetChildren())):GetRegions()):GetText(),"[%P]+") ) then
+							if(frame:IsVisible())then 
+								return frame;
+							end;
+						end;
+					end;
+				end;
+			end;
+		end;
+	end
+
+	function Pp() 
+		for n,tb in pairs(cooldown) do 
+			pl=Fp(n)
+			
+			if(pl) then 
+				if(isTarget(n)) then 
+					Bp(pl,tb,1)
+				else 
+					Bp(pl,tb,0.6)
+				end;
+			else 
+				for _,fr in pairs(tb) do 
+					fr:ClearAllPoints()
+					fr:SetPoint("Top",UIParent,"Top",0,100)
+				end;
+			end;
+		end;
+	end
+
+	function Bp(pt,tb,ap) 
+		local x=0;
+		
+		for id,fr in pairs(tb) do 
+			fr:SetAlpha(ap)
+			fr:ClearAllPoints()
+			
+			if(id==42292 or id==59752 or id==7744) then 
+				fr:SetPoint("BOTTOMLEFT",pt,"TOPRIGHT",0,-40) 
+			else 
+				fr:SetPoint("BOTTOMLEFT",pt,"TOPLEFT",x,-5);
+				x=x+ps+5;
+			end;
+		end;
+	end
+
+	function Kp(_,e,...)
+		if(e==e6) then 
+			_,eT,_,_,n,fl,_,_,_,_,_,s,_,_,_=...;
+			
+			if(isHarm(fl)~=0 and isSpell(eT)and COOLDOWN_MAP[s]) then 
+				n=strmatch(n,"[%P]+")
+				
+				--if not isReset(s,n) then 
+					SC(n,s)
+				--end;
+			end;
+		else 
+			_,st=...;
+			
+			if(e==e3 and st=="cleared") then 
+				Dp()
+			end;
+		end;
+	end
+
+	function Dp() 
+		for n,table in pairs(cooldown) do 
+			for id,frame in pairs(table) do 
+				frame:Hide()
+				frame.c:Hide()
+				_G[id..n].c=nil;
+				_G[id..n]=nil;
+			end;
+		end;
+		
+		cooldown={}
+	end
+
+	ps=30;
+	tp=0;
+	cf=CreateFrame;
+	rfc=CooldownFrame_SetTimer;
+	e3="ARENA_OPPONENT_UPDATE";
+	e6="COMBAT_LOG_EVENT_UNFILTERED";
+	f=cf("frame")
+	f:SetAllPoints()
+	f:SetScript("OnEvent",Kp)
+	f:RegisterEvent(e3)
+	f:RegisterEvent(e6)
+	f:SetScript("OnUpdate",Pp)
+end
